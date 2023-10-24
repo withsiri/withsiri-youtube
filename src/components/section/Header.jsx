eb77a386ced247ff509f83aa10c6b8c6b9b4e924
m2 <withsiri91@gmail.com>
2023-10-24 10:39:11 +0900

    섹션 컴포넌트 구조화하기

---
+++ src/components/section/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const header = () => {
+    return (
+        <header id="header" role="banner">
+            Header
+        </header>
+    )
+}
+
+export default header
