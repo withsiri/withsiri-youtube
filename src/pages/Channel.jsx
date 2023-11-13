2a515842eeefbc0f2105367777c377720802891b
m2 <withsiri91@gmail.com>
2023-11-13 19:50:15 +0900

    채널 상세 페이지 제작

--- src/pages/Channel.jsx
+++ src/pages/Channel.jsx
@@ -1,12 +1,82 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import Main from "../components/section/Main"
+import { useParams } from "react-router-dom"
+import { fetchFromAPI } from "../utils/api"
+
+import { CiBadgeDollar } from "react-icons/ci"
+import { CiMedal } from "react-icons/ci"
+import { CiRead } from "react-icons/ci"
+
+import VideoSearch from "../components/videos/VideoSearch"
+
 
 const Channel = () => {
+    const { channelID } = useParams();
+    const [ channelDetail, setChannelDetail ] = useState();
+    const [ channelVideo, setChannelVideo ] = useState([]);
+    const [ loading, setLoading ] = useState(true);
+    const [ nextPageToken, setNextPageToken ]= useState(null);
+
+    useEffect(() => {
+        const fetchResults = async () => {
+            try {
+                const data = await fetchFromAPI(`channels?part=?snippet&id=${channelID}`);
+                setChannelDetail(data.items[0]);
+
+                const videosData = await fetchFromAPI(`search?channelId=${channelID}&part=snippet%2Cid&order=date`);
+                setChannelVideo(videosData?.items);
+                setNextPageToken(videosData?.nextPageToken);                
+            } catch (error) {
+                console.log("Error fetching data", error);
+            } finally {
+                setLoading(false);
+            }
+        }
+        fetchResults();
+    }, [channelID])
+
+    const loadMoreVideos = async () => {
+        if(nextPageToken){
+            const videosData = await fetchFromAPI(`search?channelId=${channelID}&part=snippet%2Cid&order=date`);
+            setChannelVideo(prevVideos => [...prevVideos, ...videosData.items]);
+            setNextPageToken(videosData?.nextPageToken);
+        }
+    }
+
+    const channelPageClass = loading ? "isLoading" : "isLoaded";
+
     return (
         <Main
             title="유튜브 채널"
             description="유튜브 채널페이지입니다">
-            Channel
+
+
+            <section id="channel" className={channelPageClass}>
+                {channelDetail && (
+                    <div className="channel_inner">
+                        <div className="channel_header" style={{ backgroundImage: `url(${channelDetail.brandingSettings.image.bannerExternalUrl})` }}>
+                            <div className="circle">
+                                <img src={channelDetail.snippet.thumbnails.high.url} alt={channelDetail.snippet.title} />
+                            </div>
+                        </div>
+                        <div className="channel_info">
+                            <h3 className="title">{channelDetail.snippet.title}</h3>
+                            <p className="desc">{channelDetail.snippet.description}</p>
+                            <div className="info">
+                                <span><CiBadgeDollar />{channelDetail.statistics.subscriberCount}</span>
+                                <span><CiMedal />{channelDetail.statistics.videoCount}</span>
+                                <span><CiRead />{channelDetail.statistics.viewCount}</span>
+                            </div>
+                        </div>
+                        <div className="channel_video video_inner search">
+                            <VideoSearch videos={channelVideo} />  
+                        </div>
+                        <div className="channel_more">
+                            {nextPageToken && <button onClick={loadMoreVideos}>더보기</button>}
+                        </div>
+                    </div>
+                )}
+            </section>
         </Main>
     )
 }
